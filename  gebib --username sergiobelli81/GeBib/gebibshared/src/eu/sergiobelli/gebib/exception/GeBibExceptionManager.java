f628feabe84f5765822fc167cd2008e66b4f5af1
sergiobelli81 <sergiobelli81@041d5711-bf4c-0410-aa97-35387511c506>
2008-10-23 20:35:26 +0000

    modificata etichetta loggers

---  gebib --username sergiobelli81/GeBib/gebibshared/src/eu/sergiobelli/gebib/exception/GeBibExceptionManager.java
+++  gebib --username sergiobelli81/GeBib/gebibshared/src/eu/sergiobelli/gebib/exception/GeBibExceptionManager.java
@@ -24,12 +24,12 @@ public class GeBibExceptionManager {
 	public static void handleManagedException ( GeBibLogger logger, Throwable e ) {
 
 		if (Assert.isNotNull(e)) {
-			System.out.println("***** <GeComp> Catched exception : " + e.getClass().getName());
-			System.out.println("***** <GeComp> message           : " + e.getMessage());
+			System.out.println("***** <GeBib> Catched exception : " + e.getClass().getName());
+			System.out.println("***** <GeBib> message           : " + e.getMessage());
 			
 			if (Assert.isNotNull(logger)) {
-				logger.error("***** <GeComp> Catched exception : " + e.getClass().getName());
-				logger.error("***** <GeComp> message           : " + e.getMessage());
+				logger.error("***** <GeBib> Catched exception : " + e.getClass().getName());
+				logger.error("***** <GeBib> message           : " + e.getMessage());
 			}			
 		}
 		
@@ -40,17 +40,17 @@ public class GeBibExceptionManager {
 		if (Assert.isNotNull(e)) {
 
 			System.out.println("*****************************************************************");
-			System.out.println("***** <GeComp> Catched exception : " + e.getClass().getName());
+			System.out.println("***** <GeBib> Catched exception : " + e.getClass().getName());
 			e.printStackTrace();
 			System.out.println("*****************************************************************");
 			
 			if (Assert.isNotNull(logger)) {
 				logger.error("*****************************************************************");
-				logger.error("***** <GeComp> Catched exception : " + e.getClass().getName());
-				logger.error("***** <GeComp> cause             : " + e.getCause());
-				logger.error("***** <GeComp> message           : " + e.getMessage());
-				logger.error("***** <GeComp> localized message : " + e.getLocalizedMessage());
-				logger.error("***** <GeComp> trace             : ");
+				logger.error("***** <GeBib> Catched exception : " + e.getClass().getName());
+				logger.error("***** <GeBib> cause             : " + e.getCause());
+				logger.error("***** <GeBib> message           : " + e.getMessage());
+				logger.error("***** <GeBib> localized message : " + e.getLocalizedMessage());
+				logger.error("***** <GeBib> trace             : ");
 				for (StackTraceElement track : e.getStackTrace()) { logger.error(track); }
 				logger.error("*****************************************************************");
 			}
